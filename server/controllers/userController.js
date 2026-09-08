cc86d44172800d0c4541143db3ddbfa7e70ba054
Darshan Nandagavi <nandagavidarshan562@gmail.com>
2026-09-08 21:32:01 +0530

    401 error tracing

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,8 +10,8 @@ const sendTokenCookie = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
@@ -152,8 +152,8 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
   });
   res.status(200).json({ message: "Logged out successfully." });
 };
