846d10fc600f91a694840f56fca99bc973df8ffb
Darshan Nandagavi <nandagavidarshan562@gmail.com>
2026-09-08 21:45:56 +0530

    faculty error solved

--- server/controllers/facultyController.js
+++ server/controllers/facultyController.js
@@ -11,8 +11,8 @@ const sendFacultyTokenCookie = (res, facultyId) => {
 
   res.cookie("faculty_token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: none,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
@@ -163,8 +163,8 @@ export const facultyLogin = async (req, res) => {
 export const facultyLogout = async (req, res) => {
   res.clearCookie("faculty_token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
   });
   res.status(200).json({ message: "Logged out successfully." });
 };
